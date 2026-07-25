55861231352ca856413188230a20759aeebe175f
Manish12006 <ghanshanimanish152@gmail.com>
2026-07-25 09:20:50 +0530

    Updated

--- Fincore.API/Program.cs
+++ Fincore.API/Program.cs
@@ -30,7 +30,35 @@ builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Fincore.API", Version = "v1" });
+
+    options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Description = "Paste Access Token Here"
+    });
+
+    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                {
+                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(
---
+++ Fincore.Application/Constants/RoleConstants.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fincore.Application.Constants
+{
+    public class RoleConstants
+    {
+        public const string SuperAdmin = "SuperAdmin";
+        public const string Admin = "Admin";
+        public const string FinanceManager = "Finance Manager";
+        public const string ProcurementHead = "Procurement Head";
+        public const string HRManager = "HR Manager";
+        public const string DepartmentHead = "Department Head";
+        public const string Employee = "Employee";
+        public const string Vendor = "Vendor";
+        public const string Customer = "Customer";
+        public const string Accountant = "Accountant";
+        public const string Auditor = "Auditor";
+
+        // Common combinations reusable across modules
+        public const string AdminOnly = SuperAdmin + "," + Admin;
+        public const string FinanceTeam = SuperAdmin + "," + Admin + "," + FinanceManager + "," + Accountant;
+        public const string ProcurementTeam = SuperAdmin + "," + Admin + "," + ProcurementHead;
+        public const string ManagementTeam = SuperAdmin + "," + Admin + "," + DepartmentHead + "," + FinanceManager;
+    }
+}
